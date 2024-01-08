2aa9780531f34bdc9b33024969441433fbad8335
pppig236 <priv@pppig236.com>
2024-01-08 12:40:21 -0500

    [ALLY]: update patches for 6.7.0
    also update script to stop curl from printing out to console

---
+++ etc/portage/patches/sys-kernel/xanmod-sources-6.7.0/dl/dl.py
@@ -0,0 +1,31 @@
+import subprocess
+import os
+
+def download_files_with_names(url_list, output_names):
+    with open(url_list, 'r') as url_file, open(output_names, 'r') as names_file:
+        for url, custom_name in zip(url_file, names_file):
+            url = url.strip()
+            custom_name = custom_name.strip()
+            # Ignore lines starting with #
+            if url.startswith('#') or custom_name.startswith('#'):
+                continue
+
+            # Full path to the output file
+            output_file_path = os.path.join(custom_name)
+
+            # Check if the file already exists
+            if os.path.exists(output_file_path):
+                print(f"File '{custom_name}' already exists. Skipping.")
+                continue
+            # Use curl to download the file and save it with the specified name
+            subprocess.run(['curl', url, '-s', '-o', f"{custom_name}"])
+
+            # print all the files and urls to the console
+            print(f"Downloaded '{custom_name}' from '{url}'")
+
+if __name__ == "__main__":
+    url_list = "./dl/url"
+    output_names = "./dl/patch"
+    
+    download_files_with_names(url_list, output_names)
+
