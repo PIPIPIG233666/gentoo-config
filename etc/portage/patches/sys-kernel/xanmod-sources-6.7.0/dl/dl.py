1069790fe289a31c7b0da33cb5dc9a5e9bd1697d
pppig236 <priv@pppig236.com>
2024-01-08 12:53:00 -0500

    [ALLY]: update script to output log to a file

--- etc/portage/patches/sys-kernel/xanmod-sources-6.7.0/dl/dl.py
+++ etc/portage/patches/sys-kernel/xanmod-sources-6.7.0/dl/dl.py
@@ -1,31 +1,42 @@
 import subprocess
 import os
 
-def download_files_with_names(url_list, output_names):
-    with open(url_list, 'r') as url_file, open(output_names, 'r') as names_file:
-        for url, custom_name in zip(url_file, names_file):
+def download_files_with_names(url_list, output_names, log_file):
+    # Check if the number of lines in both files match
+    with open(url_list, 'r') as url_file, open(output_names, 'r') as names_file, open(log_file, 'a') as log:
+        urls = url_file.readlines()
+        names = names_file.readlines()
+
+        if len(urls) != len(names):
+            log.write("Error: The number of URLs and custom names don't match.\n")
+            return
+
+        for url, custom_name in zip(urls, names):
             url = url.strip()
             custom_name = custom_name.strip()
+
             # Ignore lines starting with #
             if url.startswith('#') or custom_name.startswith('#'):
                 continue
 
             # Full path to the output file
-            output_file_path = os.path.join(custom_name)
+            output_file_path = os.path.join("./", custom_name)
 
             # Check if the file already exists
             if os.path.exists(output_file_path):
-                print(f"File '{custom_name}' already exists. Skipping.")
+                log.write(f"File '{custom_name}' already exists. Skipping.\n")
                 continue
-            # Use curl to download the file and save it with the specified name
-            subprocess.run(['curl', url, '-s', '-o', f"{custom_name}"])
 
-            # print all the files and urls to the console
-            print(f"Downloaded '{custom_name}' from '{url}'")
+            # Use curl to download the file and save it with the specified name
+            subprocess.run(['curl', url, '-s', '-o', output_file_path])
+
+            # Write download information to the log file
+            log.write(f"Downloaded '{custom_name}' from '{url}'\n")
 
 if __name__ == "__main__":
     url_list = "./dl/url"
     output_names = "./dl/patch"
-    
-    download_files_with_names(url_list, output_names)
+    log_file = "./dl/download_log.txt"
+
+    download_files_with_names(url_list, output_names, log_file)
 
