b2501b1182b2bf8c54847fde30e035b7807367a5
Weikai Kong <priv@pppig236.com>
2025-12-20 13:32:48 -0500

    reset hyprdots
    Signed-off-by: Weikai Kong <priv@pppig236.com>

--- home/.config/hypr/scripts/RefreshNoWaybar.sh
+++ home/.config/hypr/scripts/RefreshNoWaybar.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 
 # Modified version of Refresh.sh but waybar wont refresh
@@ -25,11 +25,18 @@ for _prs in "${_ps[@]}"; do
     fi
 done
 
-# quit ags
-ags -q
+# quit ags & relaunch ags
+#ags -q && ags &
 
-# Wallust refresh
-${SCRIPTSDIR}/WallustSwww.sh &
+# quit quickshell & relaunch quickshell
+#pkill qs && qs &
+
+# Wallust refresh (synchronous to ensure colors are ready)
+${SCRIPTSDIR}/WallustSwww.sh
+sleep 0.2
+
+# reload swaync
+swaync-client --reload-config
 
 # Relaunching rainbow borders if the script exists
 sleep 1
